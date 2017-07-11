72020670fa2eee12863d788884de63c46d25e81d
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-07-11 13:44:48 +0100

    loop

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
+while true; do
+  for i in $(docker service ls -q); do
+    DOMAINS="${DOMAINS} $(docker service inspect $i --format='-d {{index .Spec.Labels "com.df.serviceDomain"}}' | grep $TLD | uniq)"
+  done
 
-for i in $(docker service ls -q); do
-  DOMAINS="${DOMAINS} $(docker service inspect $i --format='-d {{index .Spec.Labels "com.df.serviceDomain"}}' | grep $TLD | uniq)"
+  certbot --manual-public-ip-logging-ok --expand --agree-tos -m ${CERTBOT_EMAIL} -a manual ${DOMAINS} --preferred-challenges dns-01 -n --manual-auth-hook update-godaddy.sh ${EXTRA_CERTBOT_ARGS} certonly
+
+  CERT_PATH=$(find /etc/letsencrypt -name fullchain.pem)
+  PRIV_KEY=$(find /etc/letsencrypt -name privkey.pem)
+
+  cat ${CERT_PATH} ${PRIV_KEY} > /tmp/combined.pem
+
+  curl -i -XPUT \
+      --data-binary @/tmp/combined.pem \
+      "${DF_NOTIF_CERT_SERVICE_URL}?certName=combined.pem&distribute=true"
+
+  sleep ${REFRESH}
 done
-
-certbot --manual-public-ip-logging-ok --expand --agree-tos -m ${CERTBOT_EMAIL} -a manual ${DOMAINS} --preferred-challenges dns-01 -n --manual-auth-hook update-godaddy.sh ${EXTRA_CERTBOT_ARGS} certonly
-
-CERT_PATH=$(find /etc/letsencrypt -name fullchain.pem)
-PRIV_KEY=$(find /etc/letsencrypt -name privkey.pem)
-
-cat ${CERT_PATH} ${PRIV_KEY} > /tmp/combined.pem
-
-curl -i -XPUT \
-    --data-binary @/tmp/combined.pem \
-    "${DF_NOTIF_CERT_SERVICE_URL}?certName=fullchain.pem&distribute=true&redirectWhenHttpProto=true&xForwardedProto=true"
-
-sleep ${REFRESH}
