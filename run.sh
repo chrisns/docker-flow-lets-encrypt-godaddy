5130ff9b2d42d7eaedb38704743c669b17e0e1ed
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-07-14 15:45:42 +0100

    don't loop just exit on completion

--- run.sh
+++ run.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-while true; do
-  for i in $(docker service ls -q); do
-    DOMAINS="${DOMAINS} $(docker service inspect $i --format='-d {{index .Spec.Labels "com.df.serviceDomain"}}' | grep $TLD | uniq)"
-  done
-
-  certbot --manual-public-ip-logging-ok --expand --agree-tos -m ${CERTBOT_EMAIL} -a manual ${DOMAINS} --preferred-challenges dns-01 -n --manual-auth-hook update-godaddy.sh ${EXTRA_CERTBOT_ARGS} certonly
-
-  CERT_PATH=$(find /etc/letsencrypt -name fullchain.pem)
-  PRIV_KEY=$(find /etc/letsencrypt -name privkey.pem)
-
-  cat ${CERT_PATH} ${PRIV_KEY} > /tmp/combined.pem
-
-  curl -i -XPUT \
-      --data-binary @/tmp/combined.pem \
-      "${DF_NOTIF_CERT_SERVICE_URL}?certName=combined.pem&distribute=true"
-
-  echo "Sleeping for ${REFRESH}"
-  sleep ${REFRESH}
+for i in $(docker service ls -q); do
+  DOMAINS="${DOMAINS} $(docker service inspect $i --format='-d {{index .Spec.Labels "com.df.serviceDomain"}}' | grep $TLD | uniq)"
 done
+
+certbot --hsts --redirect --manual-public-ip-logging-ok --expand --agree-tos -m ${CERTBOT_EMAIL} -a manual ${DOMAINS} --preferred-challenges dns-01 -n --manual-auth-hook update-godaddy.sh ${EXTRA_CERTBOT_ARGS} certonly
+
+CERT_PATH=$(find /etc/letsencrypt -name fullchain.pem)
+PRIV_KEY=$(find /etc/letsencrypt -name privkey.pem)
+
+cat ${CERT_PATH} ${PRIV_KEY} > /tmp/combined.pem
+
+curl -i -XPUT \
+    --data-binary @/tmp/combined.pem \
+    "${DF_NOTIF_CERT_SERVICE_URL}?certName=combined.pem&distribute=true"
