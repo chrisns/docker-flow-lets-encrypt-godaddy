5f2ac9c3530dbda214d0efde9136ff11f408d41f
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-18 16:33:07 +0100

    echo the domains that are trying to be renewed

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ for i in $(docker service ls -q); do
   DOMAINS="${DOMAINS} $(docker service inspect $i --format='-d {{index .Spec.Labels "com.df.serviceDomain"}}' | grep $TLD | uniq)"
 done
 
+echo ${DOMAINS}
+
 certbot --hsts --redirect --manual-public-ip-logging-ok --expand --agree-tos -m ${CERTBOT_EMAIL} -a manual ${DOMAINS} --preferred-challenges dns-01 -n --manual-auth-hook update-godaddy.sh ${EXTRA_CERTBOT_ARGS} certonly
 
 CERT_PATH=$(find /etc/letsencrypt -name fullchain.pem)
