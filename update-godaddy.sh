fab917989e0645291325f654666e9b1d7b3cad95
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-19 00:40:30 +0000

    updating cname doesn't work, don't really care why not at the minute

--- update-godaddy.sh
+++ update-godaddy.sh
@@ -9,10 +9,10 @@ curl -kLsXPUT -H"Authorization: sso-key ${GD_KEY}:${GD_SECRET}" \
   https://api.godaddy.com/v1/domains/${TLD}/records/TXT/${NAME} \
     -d "{\"data\":\"${CERTBOT_VALIDATION}\",\"ttl\":${TTL}}"
 
-curl -kLsXPUT -H"Authorization: sso-key ${GD_KEY}:${GD_SECRET}" \
-  -H"Content-type: application/json" \
-  https://api.godaddy.com/v1/domains/${TLD}/records/CNAME/${NAME} \
-    -d "{\"data\":\"${CNAME}\",\"ttl\":${TTL}}"
+#curl -kLsXPUT -H"Authorization: sso-key ${GD_KEY}:${GD_SECRET}" \
+#  -H"Content-type: application/json" \
+#  https://api.godaddy.com/v1/domains/${TLD}/records/CNAME/${NAME} \
+#    -d "{\"data\":\"${CNAME}\",\"ttl\":${TTL}}"
 
 # give 10 seconds to allow the record to be verifiable
 sleep 10
