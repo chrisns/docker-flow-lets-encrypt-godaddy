91f976ff2e48fcc206937736d873e098d50aaae9
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-01-01 22:11:45 +0000

    hackedy hack for forcing a secret update

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ certbot --hsts --redirect --manual-public-ip-logging-ok --expand --agree-tos -m 
 CERT_PATH=$(find /etc/letsencrypt -name fullchain.pem)
 PRIV_KEY=$(find /etc/letsencrypt -name privkey.pem)
 
-cat ${CERT_PATH} ${PRIV_KEY} > /tmp/combined.pem
-
-./updateSecret.sh cert-combined.pem /tmp/combined.pem
+docker service update proxy_proxy --secret-rm=cert-combined.pem --detach=false
+docker secret rm cert-combined.pem
+cat ${CERT_PATH} ${PRIV_KEY} | docker secret create cert-combined.pem -
+docker service update proxy_proxy --secret-add=cert-combined.pem --detach=false
